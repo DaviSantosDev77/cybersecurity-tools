ea453b72565905f2dad83f06846db0bfd9aa106d
Davi santos <davi.flafla@gmail.com>
2026-02-10 23:15:03 -0300

    Delete laços.py

--- laços.py
+++
@@ -1,7 +0,0 @@
-print("Quantos anos voce tem? ")
-idades = [13,15,18,50,75]
-for idade in idades:
-    if idade >= 18:
-        print(f'{idade} é maior de idade sossegado meu fih!')
-    else:
-        print(f'{idade} é dimenor, embaçado se intera?!')
