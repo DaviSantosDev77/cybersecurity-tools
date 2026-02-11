762267d95bb9fd676478f6cd7410abe72ffb375d
Davi santos <davi.flafla@gmail.com>
2026-02-10 23:14:41 -0300

    Delete aula07.py

--- aula07.py
+++
@@ -1,9 +0,0 @@
-atrasos = int(input('Quantas faltas voce tem? '))
-if atrasos >= 3:
-    print('voce está suspenso!')
-elif atrasos == 2:
-    print('mais uma falta estará suspenso!')
-elif atrasos == 1:
-    print('mais duas faltas e estará suspenso!')
-else:
-    print('pode entrar')
